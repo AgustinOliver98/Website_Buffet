560c4a655032884fca8d6f33324eb7672f2ee34d
Agustin Oliver Gauffin <48837667+AgustinOliver98@users.noreply.github.com>
2019-04-17 11:11:54 -0300

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,47 @@
+body{
+  background: #FFFFFF;
+}
+
+header{
+  display: inline;
+  background:#112922;
+
+}
+
+.img{
+
+}
+
+.nav ul{
+  float: right;
+  pad
+}
+.nav li{
+  font-size: 25px;
+   display:inline;
+   padding: 4px;
+   border-right:1px solid #000000;
+   float: left;
+}
+.nav li:last-child{
+  border-right: none;
+}
+.nav a{
+  text-decoration: none;
+  display: block;
+  color:black;
+
+}
+<nav class=container role="menu">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText"  aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+      </button>
+      <div class"collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav menu-list list-inline">
+          <li class="list-inline-item"><a href="#">Inicio</a></li>
+          <li class="list-inline-item"><a href="#">Areas</a></li>
+          <li class="list-inline-item"><a href="#">Quienes Somos</a></li>
+          <li class="list-inline-item"><a href="#">Contacto</a></li>
+        </ul>
+      </div>
+</na
